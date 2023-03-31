f3a9d60814670838c08ec54e9c4ebc278fc72bb0
niklierik <erik200203@gmail.com>
2023-03-31 23:27:53 +0200

    Created GameScene class

--- scenes/__init__.py
+++ scenes/__init__.py
@@ -1,2 +1,4 @@
 from scenes.scene import Scene
 from scenes.viewport import Viewport
+from scenes.mainmenu import MainMenu
+from scenes.gamescene import GameScene
---
+++ scenes/gamescene.py
@@ -0,0 +1,6 @@
+from scenes import Scene
+
+
+class GameScene(Scene):
+    def __init__(self, game) -> None:
+        super().__init__(game)
