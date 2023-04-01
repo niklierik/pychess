73ec5c93343c8d588af71442833b496e2d1e925f
niklierik <erik200203@gmail.com>
2023-04-01 15:50:22 +0200

    Added basic Pawn Movement

--- game/pieces.py
+++ game/pieces.py
@@ -1,4 +1,5 @@
 import enum
+import actors.board
 
 
 class PieceColor(enum.Enum):
@@ -10,3 +11,20 @@ class Piece:
     def __init__(self, color: PieceColor) -> None:
         self.pos = (0, 0)
         self.color = color
+
+    def available_moves(self) -> list[int]:
+        return []
+
+
+class Pawn(Piece):
+    def __init__(self, color: PieceColor, file: int) -> None:
+        super().__init__(color)
+        self.pos = (file, 1 if color == PieceColor.WHITE else 6)
+        self.direction = 1 if color == PieceColor.WHITE else -1
+
+    def available_moves(self, board: actors.board.Board) -> list[int]:
+        moves = []
+        moves.append(board.index(self.pos[0] + self.direction, self.pos[1]))
+        if self.pos[0] == (1 if self.color == PieceColor.WHITE else 6):
+            moves.append(board.index(self.pos[0] + 2 * self.direction, self.pos[1]))
+        return moves
